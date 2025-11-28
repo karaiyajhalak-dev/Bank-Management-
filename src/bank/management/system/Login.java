ace2b69ba30af99117757d32f94e11c8168b1445
Sumit Singh Rajput <sumitsinghrajpootrajput@gmail.com>
2025-11-28 22:42:40 +0530

    center image

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -2,27 +2,17 @@ package bank.management.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Login extends JFrame {
 
     Login(){
         super("Bank Management System");
 
-        //ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
-        //Image i2=i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
-        //ImageIcon i3=new ImageIcon(i2);
-        //JLabel image=new JLabel(i3);
-        //image.setBounds(350,10,100,100);
-        //add(image);
-
-
-        // Frame size first
-        setSize(850,480);
-        setLocation(450,200);
-
+        setSize(850, 480);
+        setLocation(450, 200);
         setLayout(null);
 
-        // Load and scale image
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
         Image i2 = i1.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
         ImageIcon i3 = new ImageIcon(i2);
@@ -31,22 +21,33 @@ public class Login extends JFrame {
         image.setSize(120,120);
         add(image);
 
-        setVisible(true);
+        // First time center
+        centerImage(image);
 
+        // Resize hone par bhi center karne ke liye listener
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                centerImage(image);
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void centerImage(JLabel img) {
         int frameWidth = getWidth();
         int frameHeight = getHeight();
-
-        int imgWidth = image.getWidth();
-        int imgHeight = image.getHeight();
+        int imgWidth = img.getWidth();
+        int imgHeight = img.getHeight();
 
         int x = (frameWidth - imgWidth) / 2;
         int y = (frameHeight - imgHeight) / 2;
 
-        image.setLocation(x, y);
-
+        img.setLocation(x, y);
     }
+
     public static void main(String[] args) {
         new Login();
-
     }
 }
