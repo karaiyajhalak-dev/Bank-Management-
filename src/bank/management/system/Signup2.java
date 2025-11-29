76832df509b3d3e6163e122f37b75bf1be627733
khushi prajapati <khushipraj@gmail.com>
2025-11-29 22:57:10 +0530

    Initial commit

--- src/bank/management/system/Signup2.java
+++ src/bank/management/system/Signup2.java
@@ -2,9 +2,14 @@ package bank.management.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Signup2 extends JFrame {
-    JComboBox comboBox,comboBox2,comboBox3;
+public class Signup2 extends JFrame implements ActionListener{
+    JComboBox comboBox,comboBox2,comboBox3,comboBox4,comboBox5;
+    JTextField textPan,textAadhar;
+    JRadioButton r1,r2, e1,e2;
+    JButton next;;
 
     String formno;
 
@@ -66,11 +71,152 @@ public class Signup2 extends JFrame {
         comboBox3.setBounds(350,170,320,30);
         add(comboBox3);
 
+
+        JLabel l6 = new JLabel("Educational : ");
+        l6.setFont(new Font("Raleway",Font.BOLD,18));
+        l6.setBounds(100,270,150,30);
+        add(l6);
+
+        String educational[] = {"Non-graduate","graduate","Post-graduate","Doctorate","Other"};
+        comboBox4 = new JComboBox(educational);
+        comboBox4.setBackground(new Color(252,208,76));
+        comboBox4.setFont(new Font("Raleway",Font.BOLD,14));
+        comboBox4.setBounds(350,270,320,30);
+        add(comboBox4);
+
+
+        JLabel l7 = new JLabel("Occupation : ");
+        l7.setFont(new Font("Raleway",Font.BOLD,18));
+        l7.setBounds(100,340,150,30);
+        add(l7);
+
+        String occupation[] = {"Salaried","Self-Employed","Student","Retired","Other"};
+        comboBox5 = new JComboBox(occupation);
+        comboBox5.setBackground(new Color(252,208,76));
+        comboBox5.setFont(new Font("Raleway",Font.BOLD,14));
+        comboBox5.setBounds(350,340,320,30);
+        add(comboBox5);
+
+        JLabel l8 = new JLabel("PAN NUmber : ");
+        l8.setFont(new Font("Raleway",Font.BOLD,18));
+        l8.setBounds(100,390,150,30);
+        add(l8);
+
+        textPan = new JTextField();
+        textPan.setFont(new Font("Raleway", Font.BOLD,18));
+        textPan.setBounds(350,390,320,30);
+        add(textPan);
+
+        JLabel l9 = new JLabel("Aadhar Number : ");
+        l9.setFont(new Font("Raleway", Font.BOLD,18));
+        l9.setBounds(100,440,180,30);
+        add(l9);
+
+        textAadhar = new JTextField();
+        textAadhar.setFont(new Font("Raleway", Font.BOLD,18));
+        textAadhar.setBounds(350,440,320,30);
+        add(textAadhar);
+
+
+        JLabel l10 = new JLabel("Senior Citizen : ");
+        l10.setFont(new Font("Raleway", Font.BOLD,18));
+        l10.setBounds(100,490,180,30);
+        add(l10);
+
+        r1 = new JRadioButton("Yes");
+        r1.setFont(new Font("Raleway", Font.BOLD,14));
+        r1.setBackground(new Color(252,208,76));
+        r1.setBounds(350,490,100,30);
+        add(r1);
+        r2 = new JRadioButton("No");
+        r2.setFont(new Font("Raleway", Font.BOLD,14));
+        r2.setBackground(new Color(252,208,76));
+        r2.setBounds(460,490,100,30);
+        add(r2);
+
+        JLabel l11 = new JLabel("Existing Account : ");
+        l11.setFont(new Font("Raleway", Font.BOLD,18));
+        l11.setBounds(100,540,180,30);
+        add(l11);
+
+        e1 = new JRadioButton("Yes");
+        e1.setFont(new Font("Raleway", Font.BOLD,14));
+        e1.setBackground(new Color(252,208,76));
+        e1.setBounds(350,540,100,30);
+        add(e1);
+        e2 = new JRadioButton("No");
+        e2.setFont(new Font("Raleway", Font.BOLD,14));
+        e2.setBackground(new Color(252,208,76));
+        e2.setBounds(460,540,100,30);
+        add(e2);
+
+        JLabel l12 = new JLabel("Form No : ");
+        l12.setFont(new Font("Raleway", Font.BOLD,14));
+        l12.setBounds(700,10,100,30);
+        add(l12);
+
+        JLabel l13 = new JLabel(formno);
+        l13.setFont(new Font("Raleway", Font.BOLD,14));
+        l13.setBounds(760,10,60,30);
+        add(l13);
+
+        next = new JButton("Next");
+        next.setFont(new Font("Raleway",Font.BOLD,14));
+        next.setBackground(Color.WHITE);
+        next.setForeground(Color.BLACK);
+        next.setBounds(570,640,100,30);
+        next.addActionListener(this);
+        add(next);
+
         setLayout(null);
         setSize(850,750);
         setLocation(450,80);
         getContentPane().setBackground(new Color(252,208,76));
         setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String rel = (String) comboBox.getSelectedItem();
+        String cate = (String) comboBox2.getSelectedItem();
+        String inc = (String) comboBox3.getSelectedItem();
+        String edu = (String) comboBox4.getSelectedItem();
+        String occ = (String) comboBox5.getSelectedItem();
+
+        String pan = textPan.getText();
+        String addhar = textAadhar.getText();
+
+        String scitizen = " ";
+        if ((r1.isSelected())){
+            scitizen = "Yes";
+        } else if (r2.isSelected()) {
+            scitizen ="No";
+        }
+        String eAccount = " ";
+        if ((r1.isSelected())){
+            eAccount = "Yes";
+        } else if (r2.isSelected()) {
+            eAccount ="No";
+        }
+
+        try{
+            if (textPan.getText().equals("") || textAadhar.getText().equals("")){
+                JOptionPane.showMessageDialog(null,"Fill all the fields");
+            }else {
+                Connn c = new Connn();
+                String q = "insert into Signuptwo values('"+formno+"', '"+rel+"', '"+cate+"','"+inc+"','"+edu+"','"+occ+"','"+pan+"','"+addhar+"','"+scitizen+"','"+eAccount+"')";
+                c.statement.executeUpdate(q);
+                new Signup3(formno);
+                setVisible(false);
+            }
+
+
+        }catch (Exception E){
+            E.printStackTrace();
+        }
+
+
     }
 
     public static void main(String[] args) {
