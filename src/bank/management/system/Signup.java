25922ac5408975cd9b646bda5bca1721da3f36ae
jhalak karaiya <karaiyajhalak@gmail.com>
2025-11-29 13:29:50 +0530

    signup started

---
+++ src/bank/management/system/Signup.java
@@ -0,0 +1,105 @@
+package bank.management.system;
+
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Signup extends JFrame {
+
+    JTextField textName, textFname;
+    JDateChooser dateChooser;
+    JRadioButton r1,r2;
+
+    Random ran=new Random();
+    long first4=(ran.nextLong()%9000L)+1000L;
+    String first=" "+Math.abs(first4);
+
+    Signup(){
+        super("APPLICATION FORM");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        JLabel image = new JLabel(new ImageIcon(i2));
+        image.setBounds(25, 10, 100, 100);
+        add(image);
+
+        JLabel label1=new JLabel("APPLICATION FORM NO."+first);
+        label1.setBounds(160,20,600,40);
+        label1.setFont(new Font("Raleway",Font.BOLD,38));
+        add(label1);
+
+        JLabel label2=new JLabel("Page 1");
+        label2.setFont(new Font("Ralway",Font.BOLD,22));
+        label2.setBounds(330,70,600,30);
+        add(label2);
+
+        JLabel label3=new JLabel("Personl Deatils");
+        label3.setFont(new Font("Raleway",Font.BOLD,22));
+        label3.setBounds(290,90,600,30);
+        add(label3);
+
+        JLabel labelName=new JLabel("Name: ");
+        labelName.setFont(new Font("Raleway",Font.BOLD,20));
+        labelName.setBounds(100,190,100,30);
+        add(labelName);
+
+        textName = new JTextField();
+        textName.setFont(new Font("Raleway",Font.BOLD,20));
+        textName.setBounds(300,190,400,30);
+        add(textName);
+
+        JLabel labelfName=new JLabel("Father'S Name: ");
+        labelfName.setFont(new Font("Raleway",Font.BOLD,20));
+        labelfName.setBounds(100,240,200,30);
+        add(labelfName);
+
+        textFname = new JTextField();
+        textFname.setFont(new Font("Raleway",Font.BOLD,20));
+        textFname.setBounds(300,240,400,30);
+        add(textFname);
+
+        JLabel DOB=new JLabel("Date of Birth: ");
+        DOB.setFont(new Font("Raleway",Font.BOLD,20));
+        DOB.setBounds(100,340,400,30);
+        add(DOB);
+
+        dateChooser = new JDateChooser();
+        dateChooser.setForeground(new Color(105,105,105));
+        dateChooser.setBounds(300,340,400,30);
+        add(dateChooser);
+
+        JLabel labelG=new JLabel("Gender: ");
+        labelG.setFont(new Font("Raleway",Font.BOLD,20));
+        labelG.setBounds(100,290,200,30);
+        add(labelG);
+
+        r1=new JRadioButton("Male");
+        r1.setFont(new Font("Raleway",Font.BOLD,14));
+        r1.setBounds(300,290,60,30);
+        add(r1);
+
+        r2=new JRadioButton("Female");
+        r2.setFont(new Font("Raleway",Font.BOLD,14));
+        r2.setBounds(450,290,90,30);
+        add(r2);
+
+        ButtonGroup buttonGroup=new ButtonGroup();
+        buttonGroup.add(r1);
+        buttonGroup.add(r2);
+
+
+        getContentPane().setBackground(new Color(222,255,228));
+        setLayout(null);
+        setSize(850,800);
+        setLocation(360,40);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        new Signup();
+    }
+}
